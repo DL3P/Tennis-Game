d9bb20ac9023b673af8bf0ebd01ee8fb67a34ffa
DL3P <abdullah94gh@gmail.com>
2025-05-19 23:47:46 +0300

    I added a selection and also changed some things in js and made it more readable

--- app.js
+++ app.js
@@ -5,27 +5,53 @@ const ResetBtn= document.querySelector("#ResetBtn");
 const p1Display= document.querySelector("#p1Display");
 const p2Display= document.querySelector("#p2Display");
 
+const playTo= document.querySelector("#playTo");
+
 let p1Score=0;
 let p2Score=0;
-let winningScore=5;
+let winningScore=parseInt(playTo.value);
+let isGameOver=false;
 
 plus1Btn.addEventListener("click", function(){
-    if(p1Score!==winningScore && p2Score!==winningScore){
+    if(!isGameOver){
         p1Score+=1;
+        if(p1Score===winningScore){
+            isGameOver=true;
+            p1Display.classList.add("win");
+            p2Display.classList.add("lose");
+        }
         p1Display.textContent=p1Score;
     }
 })
 
 plus2Btn.addEventListener("click", function(){
-    if(p2Score!==winningScore && p1Score!==winningScore){
+    if(!isGameOver){
         p2Score+=1;
-        p2Display.textContent=p2Score;}
-    })
+        if(p2Score===winningScore){
+            isGameOver=true;
+            p2Display.classList.add("win");
+            p1Display.classList.add("lose");
+        }
+        p2Display.textContent=p2Score;
+    }
+})
 
-ResetBtn.addEventListener("click", function(){
+playTo.addEventListener("change",function(){
+    winningScore= parseInt(this.value)
+    reset()   
+})
+
+ResetBtn.addEventListener("click", reset)
+
+
+function reset(){
+    isGameOver=false;
     p1Score=0;
     p2Score=0;
     p1Display.textContent=p1Score;
     p2Display.textContent=p2Score;
-})
-
+    p1Display.classList.remove("win");
+    p2Display.classList.remove("lose");
+    p2Display.classList.remove("win");
+    p1Display.classList.remove("lose");
+}
